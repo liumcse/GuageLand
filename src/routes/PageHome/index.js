807a6165101cf0fc87cedbcb798f58f4127e1352
Mingyu Liu <mingyu.liu@grabtaxi.com>
2019-03-16 13:44:03 +0800

    Add new alias

--- src/routes/PageHome/index.js
+++ src/routes/PageHome/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import LogoImage from "../../utils/asserts/logo.png";
-import Title from "../../utils/asserts/Guageland_title.png"
+import LogoImage from "@common/assets/logo.png";
+import Title from "@common/assets/Guageland_title.png";
 
 const PageHome = () => (
   <div className="container">
