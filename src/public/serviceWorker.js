4a53b93067a363f112a3aa3d0ed91c0a5935eed2
Mingyu Liu <mingyu.liu@grabtaxi.com>
2019-03-16 20:47:48 +0800

    Service worker

--- src/public/serviceWorker.js
+++ src/public/serviceWorker.js
@@ -32,7 +32,7 @@ export function register(config) {
     }
 
     window.addEventListener("load", () => {
-      const swUrl = "./service-worker.js";
+      const swUrl = "https://fosshack.netlify.com/serviceWorker.js";
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
